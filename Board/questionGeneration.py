e043494a85dc15dc6cef22ddb218f38ecd580f9a
[Arjun] <[aatwal27@uwo.ca]>
2024-03-26 20:44:41 -0400

    Changed questionGeneration to make it easier to unit test, added unit testing for addition, subtraction, multiplication, division and a bad version of multiplication which i will fix later when im a little more drunk :D

--- Board/questionGeneration.py
+++ Board/questionGeneration.py
@@ -19,12 +19,12 @@ class questionGeneration:
 
     '''
     def __init__(self, questionType, difficulty):
-        '''Initializes the questionGeneration class with the questionType and difficulty'''
+        #Initializes the questionGeneration class with the questionType and difficulty
         self.questionType = questionType
         self.difficulty = difficulty
 
     def generateQuestion(self):
-        '''Generates a math question based on the questionType and difficulty'''
+        #Generates a math question based on the questionType and difficulty
         if self.questionType == "addition":
             return self.addition()
         elif self.questionType == "subtraction":
@@ -41,71 +41,117 @@ class questionGeneration:
             return self.linear()
 
     def addition(self):
-        '''Generates an addition question based on the difficulty'''
+        #Generates an addition question based on the difficulty
         if self.difficulty == "easy":
-            return [random.randint(0, 10), random.randint(0, 10)]
+            a = random.randint(0, 10)
+            b = random.randint(0, 10)
         elif self.difficulty == "medium":
-            return [random.randint(0, 100), random.randint(0, 100)]
+            a = random.randint(0, 100)
+            b = random.randint(0, 100)
         elif self.difficulty == "hard":
-            return [random.randint(0, 1000), random.randint(0, 1000)]
+            a = random.randint(0, 1000)
+            b = random.randint(0, 1000)
+
+        solution = a + b
+        return [f"What is {a} + {b}?",a, b, solution]
 
     def subtraction(self):
-        '''Generates a subtraction question based on the difficulty'''
+        #Generates a subtraction question based on the difficulty
         if self.difficulty == "easy":
-            return [random.randint(0, 10), random.randint(0, 10)]
+            a = random.randint(0, 10)
+            b = random.randint(0, 10)
         elif self.difficulty == "medium":
-            return [random.randint(0, 100), random.randint(0, 100)]
+            a = random.randint(0, 100)
+            b = random.randint(0, 100)
         elif self.difficulty == "hard":
-            return [random.randint(0, 1000), random.randint(0, 1000)]
+            a = random.randint(0, 1000)
+            b = random.randint(0, 1000)
+
+        solution = a - b
+        return [f"What is {a} - {b}?", a, b, solution]
 
     def multiplication(self):
-        '''Generates a multiplication question based on the difficulty'''
+        #Generates a multiplication question based on the difficulty
         if self.difficulty == "easy":
-            return [random.randint(0, 10), random.randint(0,10)]
+            a = random.randint(0, 10)
+            b = random.randint(0,10)
         elif self.difficulty == "medium":
-            return [random.randint(0, 100), random.randint(0, 100)]
+            a = random.randint(0, 100)
+            b = random.randint(0, 100)
         elif self.difficulty == "hard":
-            return [random.randint(0, 1000), random.randint(0, 1000)]
-        
+            a = random.randint(0, 1000)
+            b = random.randint(0, 1000)
+
+        solution = a * b
+        return [f"What is {a} x {b}?", a, b, solution]
+
     def division(self):
-        '''Generates a division question based on the difficulty'''
+        #Generates a division question based on the difficulty
         if self.difficulty == "easy":
-            return [random.randint(0, 10), random.randint(1,10)]
+            a = random.randint(0, 10)
+            b = random.randint(1,10)
         elif self.difficulty == "medium":
-            return [random.randint(0, 100), random.randint(1, 100)]
+            a = random.randint(0, 100)
+            b = random.randint(1, 100)
         elif self.difficulty == "hard":
-            return [random.randint(0, 1000), random.randint(1, 1000)]
-        
+            a = random.randint(0, 1000)
+            b = random.randint(1, 1000)
+
+        solution = round(a / b, 2)
+        return [f"What is {a} / {b}?\nRound to 2 decimal places", a, b, solution]
+    
     def quadratic(self):
-        '''Generates a quadratic question based on the difficulty'''
+        #Generates a quadratic question based on the difficulty, done in this way to ensure that the roots are real numbers without use of an external library
         if self.difficulty == "easy":
             a = random.randint(1, 10)
             b = random.randint(1, 10)
             c = random.randint(1, 10)
+            d = random.randint(1, 10)
         elif self.difficulty == "medium":
-            a = random.randint(1, 100)
-            b = random.randint(1, 100)
-            c = random.randint(1, 100)
+            a = random.randint(5, 15)
+            b = random.randint(5, 15)
+            c = random.randint(5, 15)
+            d = random.randint(5, 15)
         elif self.difficulty == "hard":
-            a = random.randint(1, 1000)
-            b = random.randint(1, 1000)
-            c = random.randint(1, 1000)
+            a = random.randint(5, 20)
+            b = random.randint(5, 20)
+            c = random.randint(5, 20)
+            d = random.randint(5, 20)
 
-        
-        return f"Find the roots of the function given by {a}x^2 + {b}x + {c}"
+        # from the form (ax + c)(bx + d), expand into ax^2 + bx + cx + d 
+        first = a * b
+        second = a * d + b * c
+        third = c * d
+
+        # roots are the solutions to the equation ax^2 + bx + cx + d = 0
+        roots = [round((-c) / a, 2) , round((-d) / b, 2)]
+
+        return [f"Find the roots of the function given by {first}x^2 + {second}x + {third}\nRound to 2 decimal places", first, second, third, roots]
+            
     
     def linear(self):
-        '''Generates a linear question based on the difficulty'''
+        #Generates a linear question based on the difficulty
         if self.difficulty == "easy":
             a = random.randint(1, 10)
             b = random.randint(1, 10)
+            c = random.randint(1, 10)
+            d = random.randint(1, 100)
         elif self.difficulty == "medium":
-            a = random.randint(1, 100)
-            b = random.randint(1, 100)
+            a = random.randint(5, 15)
+            b = random.randint(5, 15)
+            c = random.randint(1, 15)
+            d = random.randint(1, 100)
         elif self.difficulty == "hard":
-            a = random.randint(1, 1000)
-            b = random.randint(1, 1000)
-        return [a, b]
+            a = random.randint(5, 20)
+            b = random.randint(5, 20)
+            c = random.randint(1, 20)
+            d = random.randint(1, 100)
+
+        first = a * c * c / 2
+        second = b * c
+        constant = round(d - (first + second), 2)
+
+        return f"Determine the integral of the function given by {a}x + {b}, use ∫({c}) = {d} to find C\nRound to 2 decimal places",a,b,f"{round(a/2, 2)}x^2, {b}x + {constant}"   
     
 class addition(questionGeneration):
     '''Subclass of questionGeneration that generates addition questions'''
@@ -151,9 +197,3 @@ class linear(questionGeneration):
     def __init__(self, difficulty):
         '''Initializes the linear class with the difficulty'''
         super().__init__("linear", difficulty)
-
-
-q = questionGeneration("addition", "easy")
-print(q.generateQuestion())
-
-        
---
+++ Board/testquestionGeneration.py
@@ -0,0 +1,131 @@
+import unittest
+from questionGeneration import *
+
+class TestQuestionGeneration(unittest.TestCase):
+    def test_addition(self):
+        add = addition('easy')
+        for i in range(10):
+            question = add.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], question[1] + question[2], "Test Failed, expected: {} got: {}".format(question[3], question[1] + question[2]))
+            i += 1
+        print("Addition Easy Tests Passed\n\n")
+
+        add = addition('medium')
+        for i in range(10):
+            question = add.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], question[1] + question[2], "Test Failed, expected: {} got: {}".format(question[3], question[1] + question[2]))
+            i += 1
+        print("Addition Medium Tests Passed\n\n")
+
+        add = addition('hard')
+        for i in range(10):
+            question = add.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], question[1] + question[2], "Test Failed, expected: {} got: {}".format(question[3], question[1] + question[2]))
+            i += 1
+        print("Addition Hard Tests Passed\n\n")
+
+        print("All Addition Test Cases Passed\n\n")
+
+    def test_subtraction(self):
+        sub = subtraction('easy')
+        for i in range(10):
+            question = sub.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], question[1] - question[2], "Test Failed, expected: {} got: {}".format(question[3], question[1] - question[2]))
+            i += 1
+        print("Subtraction Easy Tests Passed\n\n")
+
+        sub = subtraction('medium')
+        for i in range(10):
+            question = sub.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], question[1] - question[2], "Test Failed, expected: {} got: {}".format(question[3], question[1] - question[2]))
+            i += 1
+        print("Subtraction Medium Tests Passed\n\n")
+
+        sub = subtraction('hard')
+        for i in range(10):
+            question = sub.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], question[1] - question[2], "Test Failed, expected: {} got: {}".format(question[3], question[1] - question[2]))
+            i += 1
+        print("Subtraction Hard Tests Passed\n\n")
+
+        print("All Subtraction Test Cases Passed\n\n")
+
+    def test_multiplication(self):
+        mult = multiplication('easy')
+        for i in range(10):
+            question = mult.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], question[1] * question[2], "Test Failed, expected: {} got: {}".format(question[3], question[1] * question[2]))
+            i += 1
+        print("Multiplication Easy Tests Passed\n\n")
+
+        mult = multiplication('medium')
+        for i in range(10):
+            question = mult.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], question[1] * question[2], "Test Failed, expected: {} got: {}".format(question[3], question[1] * question[2]))
+            i += 1
+        print("Multiplication Medium Tests Passed\n\n")
+
+        mult = multiplication('hard')
+        for i in range(10):
+            question = mult.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], question[1] * question[2], "Test Failed, expected: {} got: {}".format(question[3], question[1] * question[2]))
+            i += 1
+        print("Multiplication Hard Tests Passed\n\n")
+
+        print("All Multiplication Test Cases Passed\n\n")
+
+    def test_division(self):
+        div = division('easy')
+        for i in range(10):
+            question = div.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], round(question[1] / question[2], 2), "Test Failed, expected: {} got: {}".format(question[3], round(question[1] / question[2], 2)))
+            i += 1
+        print("Division Easy Tests Passed\n\n")
+
+        div = division('medium')
+        for i in range(10):
+            question = div.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], round(question[1] / question[2], 2), "Test Failed, expected: {} got: {}".format(question[3], round(question[1] / question[2], 2)))
+            i += 1
+        print("Division Medium Tests Passed\n\n")
+
+        div = division('hard')
+        for i in range(10):
+            question = div.generateQuestion()
+            print(question[0], "Answer should be:", question[3])
+            self.assertEqual(question[3], round(question[1] / question[2], 2), "Test Failed, expected: {} got: {}".format(question[3], round(question[1] / question[2], 2)))
+            i += 1
+        print("Division Hard Tests Passed\n\n")
+
+        print("All Division Test Cases Passed\n\n")
+
+    # Work in progress - define a range of + or -1 for the answer
+    # def test_quadratic(self):
+    #     quad = quadratic('easy')
+    #     for i in range(10):
+    #         question = quad.generateQuestion()
+    #         print(question[0], "Answer should be:", question[4])
+    #         if round(question[1]*question[4][0]*question[4][0] + question[2]*question[4][0] + question[3], 0) != 0.0:
+    #             print("Test Failed, expected: {} got: {}".format(0, question[1]*question[4][0]*question[4][0] + question[2]*question[4][0] + question[3]))
+    #         if round(question[1]*question[4][1]*question[4][1] + question[2]*question[4][0] + question[3], 0) != 0.0:
+    #             print("Test Failed, expected: {} got: {}".format(0, question[1]*question[4][1]*question[4][1] + question[2]*question[4][1] + question[3]))
+    #         i += 1
+    #     print("Quadratic Easy Tests Passed\n\n")
+            
+
+
+    
+            
+if __name__ == '__main__':
+    unittest.main()
