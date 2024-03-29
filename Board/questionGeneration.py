c22bd0b76837cdefc07a4e950b3e0e599a29d682
[Arjun] <[aatwal27@uwo.ca]>
2024-03-29 14:49:11 -0400

    got rid of whitespaces in return statements, was making testcases bug out

--- Board/questionGeneration.py
+++ Board/questionGeneration.py
@@ -55,7 +55,7 @@ class questionGeneration:
             b = random.randint(0, 1000)
 
         solution = a + b
-        return [f"What is {a} + {b}?", " "," ", solution]
+        return [f"What is {a} + {b}?"," "," ",solution]
 
     def subtraction(self):
         #Generates a subtraction question based on the difficulty
@@ -70,7 +70,7 @@ class questionGeneration:
             b = random.randint(0, 1000)
 
         solution = a - b
-        return [f"What is {a} - {b}?", " "," ", solution]
+        return [f"What is {a} - {b}?"," "," ",solution]
 
     def multiplication(self):
         #Generates a multiplication question based on the difficulty
@@ -85,7 +85,7 @@ class questionGeneration:
             b = random.randint(0, 1000)
 
         solution = a * b
-        return [f"What is {a} x {b}?", " ", " ", solution]
+        return [f"What is {a} x {b}?"," "," ",solution]
 
     def division(self):
         #Generates a division question based on the difficulty
@@ -100,7 +100,7 @@ class questionGeneration:
             b = random.randint(1, 1000)
 
         solution = round(a / b, 2)
-        return [f"What is {a} / {b}?","Round to 2 decimal places"," ", solution]
+        return [f"What is {a} / {b}?","Round to 2 decimal places"," ",solution]
     
     def quadratic(self):
         #Generates a quadratic question based on the difficulty, done in this way to ensure that the roots are real numbers without use of an external library
@@ -128,7 +128,7 @@ class questionGeneration:
         # roots are the solutions to the equation ax^2 + bx + cx + d = 0
         roots = [round((-c) / a, 2) , round((-d) / b, 2)]
 
-        return [f"Find the roots of the function given by", f"{first}x^2 + {second}x + {third}", "Round to 2 decimal places", roots]
+        return [f"Find the roots of the function given by",f"{first}x^2 + {second}x + {third}","Round to 2 decimal places",roots]
             
     
     def linear(self):
@@ -153,7 +153,7 @@ class questionGeneration:
         second = b * c
         constant = round(d - (first + second), 2)
 
-        return [f"Determine the integral of the function given by", f"{a}x + {b}, use F^(-1)({c}) = {d} to find C", "Round to 2 decimal places", f"{round(a/2, 2)}x^2, {b}x + {constant}"]
+        return [f"Determine the integral of the function given by",f"{a}x + {b}, use F^(-1)({c}) = {d} to find C", "Round to 2 decimal places",f"{round(a/2, 2)}x^2, {b}x + {constant}"]
     
     def fluidDynamics(self):
         #Generates a fluid dynamics question based on the difficulty
@@ -163,7 +163,7 @@ class questionGeneration:
             specificWeight = b * 1000 * a # specific weight = weight / volume
             density = round(specificWeight / 9.81, 2) # density = specific weight / gravity
             specificGravity = round(density / 1000, 2) # specific gravity = density of the liquid / density of water, 
-            return [f"Calculate the specific gravity of {a}L of a liquid that weighs {b}N", specificGravity]
+            return [f"Calculate the specific gravity of {a}L of a liquid that weighs {b}N",specificGravity]
         
         elif self.difficulty == "2":
             a = random.randint(1, 5)
@@ -173,7 +173,7 @@ class questionGeneration:
             totalPressure = round((c + b/2) * 9.81 * area, 2) # total pressure = wAx where w = weight density(gravity), A = area, x = depth
             massMomentofInertia = a * b * b * b / 12 # mass moment of inertia = (mass * length^2)
             centerPressure = round(massMomentofInertia / area * (c + b/2), 2) # center pressure = I / A * x, will be in m
-            return [f"Calculate the total pressure and center of pressure of a {a}m long x {b}m wide rectangular plate. It is immersed vertically in water where its 3m side is parallel to the water surface and is 1m deep below it", totalPressure, centerPressure]
+            return [f"Calculate the total pressure and center of pressure of a {a}m long x {b}m wide rectangular plate. It is immersed vertically in water where its 3m side is parallel to the water surface and is 1m deep below it",totalPressure,centerPressure]
         
         elif self.difficulty == "3":
             a = random.randint(1, 10)/10
@@ -184,7 +184,7 @@ class questionGeneration:
             velocity = round(a / area, 2) # velocity = flow rate / area
             newVelocity = round(a / newArea, 2) # new velocity = flow rate / new area
             solution = round((velocity - newVelocity)*(velocity - newVelocity) / 2 / 9.81, 2) # loss of head = (v1 - v2)^2 / 2g, will be in m of water
-            return [f"A {b}m diameter pipe carries water at a velocity of {a}m/s. If the diameter of the pipe is increased to {c}m, determine the loss of head in the pipe", solution]
+            return [f"A {b}m diameter pipe carries water at a velocity of {a}m/s. If the diameter of the pipe is increased to {c}m, determine the loss of head in the pipe",solution]
 
 class addition(questionGeneration):
     '''Subclass of questionGeneration that generates addition questions'''
